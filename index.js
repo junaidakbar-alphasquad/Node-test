457c724348acaa4e3131d1a6f9d9e6ed4caef14a
11shahzad <shahzad5135500@gmail.com>
2022-12-23 12:40:35 +0500

    test

--- index.js
+++ index.js
@@ -42,5 +42,5 @@ app.post('/login', async (req, res) => {
 //all routes
 app.use('/api', verifytoken, router)
 const PORT = process.env.PORT || 5000
-
+ 
 app.listen(PORT, () => console.log('port', PORT))
